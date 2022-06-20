f75b51b705ae38e0cb229dc8a964aad0faba0e6e
Vlad Mansurov <m_v2709@mail.ru>
2022-06-20 17:14:09 +0300

    Выполнил основное задание 16 урока

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use sctrict';
+
+class First {
+  hello() {
+    console.log(`Привет я метод родителя!`)
+  }
+}
+
+class Second extends First {
+
+  hello() {
+    super.hello();
+    console.log(`А я наследуемый метод!`);
+  }
+}
+
+const sayHello = new Second();
+sayHello.hello();
